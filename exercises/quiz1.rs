798e99dadab9a782e5ae16b434f6900bf435f13b
GG5665 <taqiyuddinarrobbaniobin@gmail.com>
2023-10-26 15:44:54 +0800

    Completed Quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apple: i32) -> i32 {
+    let mut price: i32 = 0;
+    if apple > 40 {
+        price = apple;
+    } else {
+        price = apple * 2;
+    }
+
+    return price;
+}
 
 // Don't modify this function!
 #[test]
